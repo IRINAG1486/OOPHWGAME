65715157135cbdb81d0f8c4396dbf5bb47f0199d
IRINAG <ira848@mail.ru>
2023-07-04 13:11:55 +0600

    Add Name

---
+++ game/Name.java
@@ -0,0 +1,6 @@
+package game;
+
+public enum Name {
+    Irina, Darya, Andrey, Sergey, Pavel, Ekaterina, Anton, Ivan, Artem, Marya, Olga, Anna, Maksim, Svetlana, Nadejda, Kristina, Ulia, Stepan, Nikolayi, Arkadyi;
+    
+}
